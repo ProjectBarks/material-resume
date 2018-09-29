d3020e115c0f682aa1256165c87aa77c39e4af6a
Brandon Barker <contact@brandonbarker.me>
2018-09-29 17:17:31 -0400

    card fixes, and gulp patch

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,35 @@
-var gulp = require('gulp');
-var fs = require('fs');
-var del = require('del');
-var path = require('path');
-var util = require('gulp-util');
-var jade = require('gulp-jade');
-var sass = require('gulp-sass');
-var concatJS = require('gulp-concat');
-var concatCSS = require('gulp-concat-css');
-var gulpif = require('gulp-if');
-var notify = require('gulp-notify');
-var plumber = require('gulp-plumber');
-var imagemin = require('gulp-imagemin');
-var uglify = require('gulp-uglify');
-var filter = require('gulp-filter');
-var livereload = require('gulp-livereload');
-var cleanCSS = require('gulp-clean-css');
-var uncss = require('gulp-uncss');
-var bowerFiles = require('main-bower-files');
-var jpegtran = require('imagemin-jpegtran');
+const gulp = require('gulp');
+const fs = require('fs');
+const del = require('del');
+const path = require('path');
+const util = require('gulp-util');
+const jade = require('gulp-jade');
+const sass = require('gulp-sass');
+const concatJS = require('gulp-concat');
+const concatCSS = require('gulp-concat-css');
+const gulpif = require('gulp-if');
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const filter = require('gulp-filter');
+const livereload = require('gulp-livereload');
+const cleanCSS = require('gulp-clean-css');
+const uncss = require('gulp-uncss');
+const bowerFiles = require('main-bower-files');
+const jpegtran = require('imagemin-jpegtran');
+const webserver = require('gulp-webserver');
 
-var content = './src/templates/content/bbarker.json';
-var outputDir = 'builds/';
-var env = util.env.production ? 'production' : 'development';
+const content = './src/templates/content/bbarker.json';
+const outputDir = 'builds/';
+const env = util.env.production ? 'production' : 'development';
 
 function isProduction() {
     return env === 'production';
 }
 
-gulp.task('jade', function () {
-    var contentData = JSON.parse(fs.readFileSync(content));
+gulp.task('jade', () => {
+    const contentData = JSON.parse(fs.readFileSync(content));
     return gulp.src('src/templates/**/!(_)*.jade')
         .pipe(plumber())
         .pipe(
@@ -52,38 +53,36 @@ gulp.task('jade', function () {
         .pipe(livereload())
 });
 
-gulp.task('bowerFiles', function () {
-    var jsFilter = filter('**/*.js', {restore: true});
-    var cssFilter = filter('**/*.css', {restore: true});
-    var fontFilter = filter(['**/*.eot', '**/*.woff', '**/*.woff2', '**/*.svg', '**/*.ttf'], {restore: true});
-    var vendorPath = outputDir + env + '/vendor/';
+gulp.task('bowerFiles', () => {
+    const jsFilter = filter('**/*.js', {restore: true});
+    const cssFilter = filter('**/*.css', {restore: true});
+    const fontFilter = filter(['**/*.eot', '**/*.woff', '**/*.woff2', '**/*.svg', '**/*.ttf'], {restore: true});
+    const vendorPath = `${outputDir + env}/vendor/`;
 
 
-    return gulp.src(bowerFiles(), {
-            base: 'bower_components'
-        })
+    return gulp.src(bowerFiles(), { base: 'bower_components' })
         .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
         //JS
         .pipe(jsFilter)
         .pipe(concatJS("dist.js"))
         .pipe(gulpif(isProduction(), uglify()))
-        .pipe(gulp.dest(vendorPath + "js"))
+        .pipe(gulp.dest(`${vendorPath}js`))
         .pipe(jsFilter.restore)
         //CSS
         .pipe(cssFilter)
         .pipe(concatCSS("dist.css", {rebaseUrls: false}))
         .pipe(gulpif(isProduction(), uncss({
-            html: [outputDir + env + "/index.html"]
+            html: [`${outputDir + env}/index.html`]
         })))
         .pipe(gulpif(isProduction(), cleanCSS()))
-        .pipe(gulp.dest(vendorPath + "css"))
+        .pipe(gulp.dest(`${vendorPath}css`))
         .pipe(cssFilter.restore)
         //Fonts
         .pipe(fontFilter)
-        .pipe(gulp.dest(function(file) {
-            var parentName = path.basename(path.dirname(file.path));
+        .pipe(gulp.dest(file => {
+            const parentName = path.basename(path.dirname(file.path));
             file.path = path.join(file.base, path.basename(file.path));
-            return path.join(vendorPath + 'fonts', parentName.toLowerCase().indexOf("font") <= -1 ? parentName : "");
+            return path.join(`${vendorPath}fonts`, parentName.toLowerCase().indexOf("font") <= -1 ? parentName : "");
         }))
         .pipe(fontFilter.restore)
         .pipe(notify({
@@ -95,69 +94,61 @@ gulp.task('bowerFiles', function () {
         .pipe(livereload());
 });
 
-gulp.task('sass', function () {
-    return gulp.src('src/sass/**/!(_)*.scss')
-        .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
-        .pipe(
-            gulpif(isProduction(),
-                sass({outputStyle: 'compressed'}),
-                sass({sourceComments: 'map'})
-            )
+gulp.task('sass', () => gulp.src('src/sass/**/!(_)*.scss')
+    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+    .pipe(
+        gulpif(isProduction(),
+            sass({outputStyle: 'compressed'}),
+            sass({sourceComments: 'map'})
         )
-        .pipe(gulp.dest(outputDir + env + '/css/'))
-        .pipe(notify({
-            message: '<%= file.relative %> created successfuly',
-            templateOptions: {
-                date: new Date()
-            }
-        }))
-        .pipe(livereload())
-});
+    )
+    .pipe(gulp.dest(`${outputDir + env}/css/`))
+    .pipe(notify({
+        message: '<%= file.relative %> created successfuly',
+        templateOptions: {
+            date: new Date()
+        }
+    }))
+    .pipe(livereload()));
 
-gulp.task('images', function () {
-    return gulp.src('src/assets/images/**/!(_)*.*')
-        /*.pipe(imagemin({
-            plugins: [jpegtran()]
-        }))*/
-        .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
-        .pipe(gulp.dest(outputDir + env + '/images/'))
-        .pipe(notify({
-            message: '<%= file.relative %> created successfuly',
-            templateOptions: {
-                date: new Date()
-            }
-        }))
-        .pipe(livereload())
-});
+gulp.task('images', () => gulp.src('src/assets/images/**/!(_)*.*')
+    /*.pipe(imagemin({
+        plugins: [jpegtran()]
+    }))*/
+    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+    .pipe(gulp.dest(`${outputDir + env}/images/`))
+    .pipe(notify({
+        message: '<%= file.relative %> created successfuly',
+        templateOptions: {
+            date: new Date()
+        }
+    }))
+    .pipe(livereload()));
 
-gulp.task('downloads', function () {
-    return gulp.src('src/assets/downloads/**/!(_)*.*')
-        .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
-        .pipe(gulp.dest(outputDir + env + '/downloads/'))
-        .pipe(notify({
-            message: '<%= file.relative %> created successfuly',
-            templateOptions: {
-                date: new Date()
-            }
-        }))
-        .pipe(livereload())
-});
+gulp.task('downloads', () => gulp.src('src/assets/downloads/**/!(_)*.*')
+    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+    .pipe(gulp.dest(`${outputDir + env}/downloads/`))
+    .pipe(notify({
+        message: '<%= file.relative %> created successfuly',
+        templateOptions: {
+            date: new Date()
+        }
+    }))
+    .pipe(livereload()));
 
-gulp.task('javascript', function () {
-    return gulp.src('src/js/*.*')
-        .pipe(plumber())
-        .pipe(gulpif(isProduction(), uglify()))
-        .pipe(gulp.dest(outputDir + env + '/js/'))
-        .pipe(notify({
-            message: '<%= file.relative %> created successfuly',
-            templateOptions: {
-                date: new Date()
-            }
-        }))
-        .pipe(livereload())
-});
+gulp.task('javascript', () => gulp.src('src/js/*.*')
+    .pipe(plumber())
+    .pipe(gulpif(isProduction(), uglify()))
+    .pipe(gulp.dest(`${outputDir + env}/js/`))
+    .pipe(notify({
+        message: '<%= file.relative %> created successfuly',
+        templateOptions: {
+            date: new Date()
+        }
+    }))
+    .pipe(livereload()));
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
     livereload.listen();
     gulp.watch('src/js/**/*.*', ['javascript']);
     gulp.watch('src/templates/**/*.*', ['jade']);
@@ -167,7 +158,14 @@ gulp.task('watch', function () {
     gulp.watch('src/assets/downloads/**/!(_)*.*', ['downloads']);
 });
 
-gulp.task('default', ['bowerFiles', 'sass', 'jade', 'images', 'downloads', 'javascript', 'watch']);
+gulp.task('start', ['default', 'watch'], () => {
+    gulp.src('builds/development')
+        .pipe(webserver({
+            fallback: 'index.html',
+            open: true
+        }));
+});
 
+gulp.task('default', ['bowerFiles', 'sass', 'jade', 'images', 'downloads', 'javascript']);
 
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,19 @@
-"use strict";
-$(document).ready(function () {
+$(document).ready(() => {
     ////////////////////////////////
     //        Introduction        //
     ////////////////////////////////
     function easyType(item, callback) {
-        console.log(item, item.attr("data"));
         item.typed({
             strings: [item.attr("data")],
             showCursor: false,
-            callback: callback || function () {}
+            callback: callback || (() => {})
         })
     }
 
     function disableLoader() {
-        var wrapper = $(".load-wrapper");
+        const wrapper = $(".load-wrapper");
         $("html").removeClass("disable-scroll");
-        wrapper.fadeOut(250, function () {
+        wrapper.fadeOut(250, () => {
             wrapper.css({display: "none"});
         });
     }
@@ -24,16 +22,15 @@ $(document).ready(function () {
 
         // Counting the perceptive luminance
         // human eye favors green color...
-        var a = 1 - (0.299 * r + 0.587 * g + 0.114 * b) / 255;
-        console.log(a);
+        const a = 1 - (0.299 * r + 0.587 * g + 0.114 * b) / 255;
         return (a < 0.5) ? '#212121' : '#ffffff';
     }
 
     if (Cookies.get("visited") == null) {
-        easyType($(".loader .prelabel"), function () {
-            setTimeout(function () {
-                var label = $(".loader .label");
-                easyType(label, function () {
+        easyType($(".loader .prelabel"), () => {
+            setTimeout(() => {
+                const label = $(".loader .label");
+                easyType(label, () => {
                     label.append("<span class='typed'></span>");
                     $(".typed").typed({
                         strings: $(".typed-strings span").map(function () {
@@ -54,20 +51,19 @@ $(document).ready(function () {
     ////////////////////////////////
     //     Side Bar Controls      //
     ////////////////////////////////
-    var sideNav = $("#side-nav");
-    var portraitWrapper = sideNav.find(".portrait-wrapper");
+    const sideNav = $("#side-nav");
+    const portraitWrapper = sideNav.find(".portrait-wrapper");
 
     sideNav.on('transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd',
         () => {
-            console.log("end")
             sideNav.removeClass("animate");
             sideNav.removeClass("animate-out");
         }
     );
 
-    var scrollMode = {
+    const scrollMode = {
         enabled: false,
-        enable: function () {
+        enable() {
             if ($(document).width() <= 600) 
                 return;
             sideNav.addClass("animate");
@@ -78,7 +74,7 @@ $(document).ready(function () {
             scrollMode.checkMargins();
             scrollMode.enabled = true;
         },
-        disable: function() {
+        disable() {
             sideNav.addClass("animate");
             sideNav.removeClass("mini");
 
@@ -86,13 +82,13 @@ $(document).ready(function () {
                 sideNav.addClass("animate-out");
             scrollMode.enabled = false;
         },
-        checkMargins: function() {
+        checkMargins() {
             if ($(document).width() <= 600) {
                 scrollMode.disable();
             }
         },
-        refresh: function() {
-            var scroll = $(window).scrollTop();
+        refresh() {
+            const scroll = $(window).scrollTop();
             if(scroll >= portraitWrapper.offset().top) {
                 scrollMode.enable();
             }
@@ -106,12 +102,12 @@ $(document).ready(function () {
         if ($(document).width() <= 600) {
             sideNav.css({height: ""});
         } else {
-            var main = $("main");
-            sideNav.css({height: main.height() + "px"});
+            const main = $("main");
+            sideNav.css({height: `${main.height()}px`});
         }
     }
 
-    $(window).resize(function () {
+    $(window).resize(() => {
         checkSidebarHeight();
         if (!scrollMode.enabled) {
             return;
@@ -130,19 +126,19 @@ $(document).ready(function () {
     ////////////////////////////////
     //     Card Controls & Color  //
     ////////////////////////////////
-    var lastClicked = null;
+    let lastClicked = null;
 
     $("#projects").find(".card").each(function() {
-        var card = $(this);
+        const card = $(this);
 
-        card.find(".activator").click(function() {
+        card.find(".activator").click(() => {
             if (lastClicked != null) {
                 lastClicked.find(".disabler").click();
             }
             lastClicked = card;
         });
 
-        card.find(".disabler").click(function() {
+        card.find(".disabler").click(() => {
             lastClicked.find(".waves-ripple").remove();
         });
     });
@@ -152,8 +148,8 @@ $(document).ready(function () {
     ////////////////////////////////
     $("a[href*=\"#\"]:not([href=\"#\"])").click(function() {
         if (location.pathname.replace(/^\//,"") == this.pathname.replace(/^\//,"") && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $("[name=" + this.hash.slice(1) +"]");
+            let target = $(this.hash);
+            target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
             if (target.length) {
                 $("html, body").animate({
                     scrollTop: target.offset().top
